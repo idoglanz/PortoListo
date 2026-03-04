b25cd426efad4664a19615ec83d00d836761b088
Ido Glanz <idoglanz@gmail.com>
2026-03-04 09:17:11 +0200

    Initial Commit

---
+++ PortoListo/PortoListoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortoListoApp.swift
+//  PortoListo
+//
+//  Created by Ido Glanz on 04/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct PortoListoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
